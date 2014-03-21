cac4d57f2b5a7ebef8ea91c60fc297ec4fa325b4
Hüseyin Bozkurt <hbozkurt@student.tgm.ac.at>
2014-03-21 11:35:03 +0100

    Der Erste Schritt :D

---
+++ HelloWorldTeam/src/Bozkurt/HelloWorld.java
@@ -0,0 +1,14 @@
+package Bozkurt;
+
+public class HelloWorld {
+	
+	public static void bozzy() {
+		System.out.println("Grüßt euch! - Bozkurt :)");
+	}
+
+	public static void main(String[] args) {
+
+		HelloWorld hw = new HelloWorld();
+		hw.bozzy();
+}
+}
